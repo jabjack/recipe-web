ec0821ff5c3e847b7dc98d8cbbaddd209f2f9afe
jabjack <jsperez9988@gmail.com>
2023-04-19 15:47:48 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+const getElement = (selector) => {
+
+    const element = document.querySelector(selector)
+
+    if(element) return element
+    throw Error(
+
+        `no hay selector en en double check is ${selector} class`
+    )
+
+}
+
+
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+
+
+navBtnDOM.addEventListener('click', () => {
+
+    links.classList.toggle('show-links')
+})
+
+const date = getElement('#date');
+
+const currentYear = new Date().getFullYear()
+
+date.textContent = currentYear
+
